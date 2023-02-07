7b20a919a6ed3b89c4a8da99449a07b9201f00b2
asheshpande <asheshpande9@gmail.com>
2023-02-07 10:39:11 +0530

    first commit

---
+++ a.py
@@ -0,0 +1,6 @@
+name="ash"
+age="77"
+adult= True
+
+print(name)
+print(adult)
---
+++ sample.py
@@ -0,0 +1,10 @@
+for i in range(10,0,-1):
+    print("i: ",i)
+    
+i= int(input("enter value: "))
+print("enter value: ",i)
+print("****************")
+
+for i in range(0,10):
+ i= int(input("enter value: "))
+ print(" ",i)
